d751a42d95d5d364fef963316eccaed3bab4c5cf
rtink-git <rtink.git@gmail.com>
2023-04-10 21:47:39 +0300

    Page - I / Delete - script - buffer.js

--- wwwroot/PageComponents/HeaderDescriptionHtmlBox/script.js
+++ wwwroot/PageComponents/HeaderDescriptionHtmlBox/script.js
@@ -1,30 +1,34 @@
 ﻿export class HeaderDescriptionHtmlBox {
-    constructor(target, position, main, sub) {
+    constructor(target, position, main, sub, location) {
         let name = "HeaderDescriptionHtmlBox"
         let url = "/PageComponents/" + name;
         let css = document.createElement("link"); css.setAttribute("rel", "stylesheet"); css.setAttribute("href", url + "/style.css"); document.head.append(css);
 
         let mainHtml = "";
-        for (var i = 0; i < main.length; i++) {
+        for (var i = 0; i < main.length; i++)
             mainHtml += "\
-        <div>\
-            <span>" + main[i] + "</span>\
-        </div>"
-        }
+            <div>\
+                <span>" + main[i] + "</span>\
+            </div>"
+
         if (mainHtml.length > 0)
             mainHtml = "<div>" + mainHtml + "</div>";
 
         let subHtml = ""
-        for (var i = 0; i < sub.length; i++) {
+        for (var i = 0; i < sub.length; i++)
             subHtml += "\
-        <div>\
-            <span>" + sub[i] + "</span>\
-        </div>"
-        }
+            <div>\
+                <span>" + sub[i] + "</span>\
+            </div>"
+
+        let locationHtml = ""
+        if (location != null && location.length > 0)
+            locationHtml = "<div><a>Russia</a></div>"
+
         if (subHtml.length > 0)
             subHtml = "<div>" + subHtml + "</div>";
 
-        let html = "<div id=\"" + name + "\">" + mainHtml + subHtml + "</div>"
+        let html = "<div id=\"" + name + "\">" + mainHtml + subHtml + locationHtml + "</div>"
         target.insertAdjacentHTML(position, html)
     }
 }
