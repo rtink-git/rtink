c9166cac4fafb1d401e562921704d3e67ef1cd08
rtink-git <129110347+rtink-git@users.noreply.github.com>
2023-05-03 06:15:05 +0500

    Pages / Locations - New H1 Header

--- wwwroot/Pages/Locations/script.js
+++ wwwroot/Pages/Locations/script.js
@@ -1,6 +1,6 @@
 ﻿import { isTest, apiUrl, PageHeadsBuild, authJWToken, RoleId } from '/PageComponents/Page/script.js';
 import { HeaderHtmlBox } from '/PageComponents/HeaderHtmlBox/script.js';
-import { HeaderDescriptionHtmlBox } from '/PageComponents/HeaderDescriptionHtmlBox/script.js';
+//import { HeaderDescriptionHtmlBox } from '/PageComponents/HeaderDescriptionHtmlBox/script.js';
 //import { SearchHeaderQHtmlBox } from "/PageComponents/SearchHeaderQHtmlBox/script.js";
 import { LocsHtmlBox } from '/PageComponents/LocsHtmlBox/script.js';
 
@@ -21,12 +21,28 @@ if (RoleId == 0)
     href = "/"
 menuList.push({ "icon": LocationsPageUrlContent + "/undo.png", "href": href });
 
-new HeaderHtmlBox(document.getElementsByTagName("body")[0], "afterbegin", "RT", null, menuList, isTest)
-new HeaderDescriptionHtmlBox(document.getElementById("HeaderHtmlBox"), "afterend", "LOCATIONS", "")
+new HeaderHtmlBox(document.getElementsByTagName("body")[0], "afterbegin", "", null, menuList, isTest)
+//new HeaderDescriptionHtmlBox(document.getElementById("HeaderHtmlBox"), "afterend", "LOCATIONS", "")
 
+let htmlQ = "\
+<div id=\"H1QHtmlBox\">\
+    <div>\
+        <h1>\
+            <span>\
+                LOCATIONS\
+            </span>\
+        </h1>\
+        <p>\
+            watch & choose\
+        </p>\
+    </div>\
+</div>\
+"
+
+document.getElementById("HeaderHtmlBox").insertAdjacentHTML("afterend", htmlQ)
 //new SearchHeaderQHtmlBox(document.getElementById("HeaderHtmlBox"), "afterend", "", "", "")
 
-let locsHtmlBox = new LocsHtmlBox(document.getElementById("HeaderDescriptionHtmlBox"), "afterend", apiUrl, authJWToken, RoleId)
+let locsHtmlBox = new LocsHtmlBox(document.getElementById("H1QHtmlBox"), "afterend", apiUrl, authJWToken, RoleId)
 await locsHtmlBox.AppendBaseList()
 
 
